42ddecbdccb88121810ca6239fff942beb2043ed
Jonas Åsnes  Sagild <jonassagild@hotmail.com>
2023-06-18 00:24:25 +0200

    fix spelling mistake

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
-from routers import max_wave_heigh
+from routers import max_wave_height
 
 app = FastAPI()
 
-app.include_router(max_wave_heigh.router)
+app.include_router(max_wave_height.router)
