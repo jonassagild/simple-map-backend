de36357325af259663b38fcbee516231f2683002
Jonas Åsnes  Sagild <jonassagild@hotmail.com>
2023-06-21 22:38:04 +0200

    add all origins accepted in cors

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
+from fastapi.middleware.cors import CORSMiddleware
+
 from fastapi import FastAPI
 from routers import max_wave_height
 
+
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(max_wave_height.router)
